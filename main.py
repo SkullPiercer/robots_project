1f64a480f89fa42f5f25b81ae81c98bd65dfa2d4
Александр Денисов <akatsuki_sanya@vk.com>
2024-01-19 12:47:06 +0600

    Add reviewers marks

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 # 105168265
-def min_platforms(weights: list, limit: int) -> int:
-    '''Функция для определения кол-ва необходимых платформ.'''
-    # Сортируем массив с роботами.
+def min_platforms(weights: list[int], limit: int) -> int:
+    """Функция для определения кол-ва необходимых платформ."""
     weights.sort()
     platforms = 0
-    # Воспользуемся методом двух указателей.
     left, right = 0, len(weights) - 1
     while left <= right:
-        # Если на платформе можно уместить два робота.
         if weights[left] + weights[right] <= limit:
-            # Смещаем оба указателя
             left += 1
-            right -= 1
-        else:
-            # Иначе смещаем только тяжелого.
-            right -= 1
+        right -= 1
         platforms += 1
     return platforms
 
 
 if __name__ == '__main__':
-    robots_weights = list(map(int, input().split()))
+    robots_weights = input()
+    robots_list = [int(i) for i in robots_weights.split()]
     platform_limit = int(input())
-    result = min_platforms(robots_weights, platform_limit)
+    result = min_platforms(robots_list, platform_limit)
     print(result)
